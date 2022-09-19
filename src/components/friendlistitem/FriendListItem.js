2ba9121b9cb138110bc4c7b6d62db56379019d33
Veronika Savluk <s_veronica@ukr.net>
2022-09-19 19:40:54 +0300

    added module.css

--- src/components/friendlistitem/FriendListItem.js
+++ src/components/friendlistitem/FriendListItem.js
@@ -1,18 +1,12 @@
-// import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
-    return (<li className="item">
-        <span className="status">{isOnline}</span>
-  <img className="avatar" src={avatar} alt={name} width="48" />
-        <p className="name">{name}</p>
+    return (<li className={css.item}>
+        <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+        <img className={css.avatar} src={avatar} alt={name} width="48" />
+        <p className={css.name}>{name}</p>
 </li>
 )
 };
-
-// FriendListItem.propTypes = {
-//     avatar: PropTypes.string.isRequired,
-//     name: PropTypes.string.isRequired,
-//     isOnline: PropTypes.bool.isRequired,
-// };
 
 export default FriendListItem;

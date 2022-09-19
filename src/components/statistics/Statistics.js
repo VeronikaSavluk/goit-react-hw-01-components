f59f0288afb9dc073ba14694274200309ffe7136
Veronika Savluk <s_veronica@ukr.net>
2022-09-19 19:42:39 +0300

    added module.css&corrected PropTypes

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,22 +1,33 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 
 const Statistics = ({ title, stats }) => {
-    return (<section className="statistics">
-        {title && <h2 className="title">{title}</h2>}
-        {stats && <ul className="stat-list">
-            {stats.map(stat => (
-                <li className="item" key={stat.id}>
-                    <span className="label">{stat.label}</span>
-                    <span className="percentage">{stat.percentage}%</span>
+
+    return (<section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+        {stats && <ul className={css.statList}>
+            {stats.map(stat => {
+                const colorItem = {
+                    backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`,
+                };
+                return (<li className={css.item} style={colorItem} key={stat.id}>
+                    <span className={css.label}>{stat.label}</span>
+                    <span className={css.percentage}>{stat.percentage}%</span>
                 </li>
-            ))}
+                )
+            })}
         </ul>}
     </section>);
 };
 
 Statistics.propTypes = {
+    stats: PropTypes.arrayOf(
+    PropTypes.exact({
+    id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     percentage: PropTypes.number.isRequired,
+    })
+    ),
 };
 
 export default Statistics;

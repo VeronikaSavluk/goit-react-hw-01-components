0d756bfb5cc43c3c2497d15f714224e9655405a7
Veronika Savluk <s_veronica@ukr.net>
2022-09-19 19:37:17 +0300

    added module.css&corrected PropTypes

--- src/components/transactionhistory/TransactionHistory.js
+++ src/components/transactionhistory/TransactionHistory.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => {
-    return (
-        <table class="transaction-history">
+  return (
+    <table className={css.transactionHistory}>
   <thead>
     <tr>
       <th>Type</th>
@@ -24,11 +25,14 @@ const TransactionHistory = ({ items }) => {
 }
 
 TransactionHistory.propTypes = {
+    stats: PropTypes.arrayOf(
+    PropTypes.exact({
     id: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     amount: PropTypes.number.isRequired,
-    currency: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+    ),
 }
 
 export default TransactionHistory;
-
